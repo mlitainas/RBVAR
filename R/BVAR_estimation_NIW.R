aaa0c89dc42cfa43ed61ecf8cf53109d019ef09d
mlitainas <michael.litainas@gmail.com>
2023-07-09 20:19:37 +0100

    TV-COV matrix option

--- R/BVAR_estimation_NIW.R
+++ R/BVAR_estimation_NIW.R
@@ -1,8 +1,7 @@
-BVAR_estimation_NIW = function(data, lags = 4, reps = 1000, burn = 500, const = T, trend = F, trend_qua = F, ex = NULL, ex_lag = NULL, xlags = 0,dummy = dummy, lamda = 1 ,tau = 4  , epsilon = 1) {
+BVAR_estimation_NIW = function(data, lags = 4, reps = 1000, burn = 500, const = T, trend = F, trend_qua = F, ex = NULL, ex_lag = NULL, xlags = 0,dummy = dummy, lamda = 1 ,tau = 4  , epsilon = 1, TVV = FALSE) {
 
-  #lags =4; reps = 1000; burn = 500 ;const= T;trend =F; trend_qua = F;  ex = NULL ;  xlags = 4; i=1; ex_lag = NULL;lamda=1;tau=0; epsilon=0.0001
-  #data = df[1:100,c("LGSPEN","GDP_OECD_IX")]
-  #data =  us %>% filter(Date > "1982-01-01" & Date < "2020-01-01") %>%  select(lgs_pc, NT13, ltax_pc, lgdp_pc, lcon_pc)
+  #lags = 2; reps = 300; burn = 50 ;const= T;trend =F; trend_qua = F;  ex = NULL ;  xlags = 0; i=1; ex_lag = NULL;lamda= 0.3;tau=0; epsilon=0.0001; dummy = NULL; TVV = T
+  #data =  us %>% filter(Date > "1982-01-01" & Date < "2020-01-01") %>%  select(Date, lgs_pc,NT13, ltax_pc, lgdp_pc, lr, lre)
   # vardata = df1$dfr[[3]]
   # ex_lag = as.data.frame(vardata) %>% select_if(~ !any(is.na(.))) %>% select(ends_with("_s"), -timeID)
   # data = as.data.frame(vardata) %>% select_if(~ !any(is.na(.))) %>% select(-ends_with("_s"), -timeID)
@@ -45,9 +44,25 @@ BVAR_estimation_NIW = function(data, lags = 4, reps = 1000, burn = 500, const = 
 
     # draw covariance
     e = Y0 - X0 %*% matrix(beta,nrow = n*L+ex, ncol = n)
-
-    scale = crossprod(e)
-
+    
+    if (TVV){
+      #tvReg::bwCov(as.matrix(e))
+      Sigma.hat = tvReg::tvCov( x = as.matrix(e),est = "lc", bw = 0.14)
+      #sigma_tv = matrix(0, ncol = n, nrow = t_lag)
+      # for (ii in 1:t_lag){
+      #   sigma_tv[ii, ] = diag(Sigma.hat[,,ii])  
+      #   
+      # }
+      #plot(y = sigma_tv[,5], x = 1:t_lag)
+      
+      ##The mean over time of all estimates
+      scale = apply(Sigma.hat, 1:2, mean)
+      
+    } else {
+        
+      scale = crossprod(e)
+    
+    }
     sigma = MCMCpack::riwish( (t_lag + nrow(Y0)) , scale)  #flag
 
     if (i > burn) {
@@ -61,7 +76,7 @@ BVAR_estimation_NIW = function(data, lags = 4, reps = 1000, burn = 500, const = 
 
   }
 
-  arguments = list("tau" = tau, "lamda" = lamda, "epsilon" =  epsilon)
+  arguments = list("tau" = tau, "lamda" = lamda, "epsilon" =  epsilon, "TVV" = TVV)
   bvar = list(beta_s, Sigma, CM, res, fit, vardata, draws, arguments)
   names(bvar) = c("posterior","Sigma", "CM", "res", "yfit", "vardata", "draws","priors")
   return(bvar)
